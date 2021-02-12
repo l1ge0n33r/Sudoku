d3821e72be4219534b8ff2e5b0d0b697de5902fa
l1ge0n33r <48294455+l1ge0n33r@users.noreply.github.com>
2021-02-12 11:29:07 +0300

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -40,10 +40,10 @@ void fillSlv()
 
 bool isOverLayed(int row, int col, int val)
 {
-    if (val != 0) 
+    if (val != 0)
     {
         if (grid[row][col] != val)
-        {    
+        {
             return false;
         }
         else return true;
@@ -53,36 +53,36 @@ bool isOverLayed(int row, int col, int val)
 
 int isPresentInRow(int row, int num)
 {
-    
+
     for (int col = 0; col < 9; col++) {
         if (slv_grid[row][col] == num) return 1;
     }
-    
+
     return 0;
 }
 
 int isPresentInCol(int col, int num)
 {
-    
+
     for (int row = 0; row < N; row++) {
         if (slv_grid[row][col] == num) return 2;
     }
-    
+
     return 0;
 }
 
 int isPresentInBox(int boxStartRow, int boxStartCol, int num)
 {
-    
-    for (int row = 0; row < 3; row++) 
+
+    for (int row = 0; row < 3; row++)
     {
-        for (int col = 0; col < 3; col++) 
+        for (int col = 0; col < 3; col++)
         {
             if (slv_grid[row + boxStartRow][col + boxStartCol] == num)
                 return 4;
         }
     }
-    
+
     return 0;
 }
 
@@ -96,15 +96,15 @@ bool findEmptyPlace(int& row, int& col)
 }
 
 int isValidPlace(int row, int col, int num) {
-    
-    if (num == -1) { return 0;}
+
+    if (num == -1) { return 0; }
 
     else return isPresentInRow(row, num) + isPresentInCol(col, num) + isPresentInBox(row - row % 3, col - col % 3, num);
 }
 
 bool isSolved()
 {
-    for (int i = 0; i < N; i++) 
+    for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
@@ -117,9 +117,9 @@ bool isSolved()
 }
 
 void sudokuGrid() {
-    for (int row = 0; row < N; row++) 
+    for (int row = 0; row < N; row++)
     {
-        for (int col = 0; col < N; col++) 
+        for (int col = 0; col < N; col++)
         {
             if (col == 3 || col == 6)
                 cout << " | ";
@@ -138,58 +138,70 @@ void sudokuGrid() {
 
 void Game()
 {
-    int valout=0;
-    int row=0, column=0, value=-1;
+    int valout = 0;
+    int row = 0, column = 0, value = -1;
     char c;
 idk:
     system("cls");
 
     sudokuGrid();
-    cout << "\n\n Use q to quit game \n Use s to show solution(WIP, it's not working) \n Use 0 to erase number in place\n Use e to enter number\n";
+    cout << "\n\n Use q to quit game \n Use 0 to erase number in place\n Use e to enter number\n";
     cout << " Enter char of action: ";
     cin >> c;
 
     if (c == 'q')gameover = true;
     else if (c == 's') goto idk;
-	else if(c=='e'){
-	
-	system("cls");
-    sudokuGrid();
-    cout << "Enter row: ";
-	cin>>row;
-    cout << "Enter column: ";
-	cin>>column;
-    cout << "Enter value: ";
-	cin>>value;
-
-    
-    valout = isValidPlace(row-1, column-1, value);
-
-
-    if (valout != 0 && !isOverLayed(row-1, column-1, value))
+    else if (c == '0') 
     {
         system("cls");
-
         sudokuGrid();
-        cout << "The number is already in: ";
-        if (valout & 0b001) cout << "row ";
-        if (valout & 0b010) cout << "column ";
-        if (valout & 0b100) cout << "box";
-        
+        cout << "Enter row: ";
+        cin >> row;
+        cout << "Enter column: ";
+        cin >> column;
+
+        slv_grid[row-1][column-1] = 0;
+
     }
-    else 
-    {
-        slv_grid[row-1][column-1] = value;
+    else if (c == 'e') {
+
+        system("cls");
+        sudokuGrid();
+        cout << "Enter row: ";
+        cin >> row;
+        cout << "Enter column: ";
+        cin >> column;
+        cout << "Enter value: ";
+        cin >> value;
+
+
+        valout = isValidPlace(row - 1, column - 1, value);
+
+
+        if (valout != 0 && !isOverLayed(row - 1, column - 1, value))
+        {
+            system("cls");
+
+            sudokuGrid();
+            cout << "The number is already in: ";
+            if (valout & 0b001) cout << " row";
+            if (valout & 0b010) cout << " column";
+            if (valout & 0b100) cout << " box";
+
+        }
+        else
+        {
+            slv_grid[row - 1][column - 1] = value;
+        }
+        system("pause");
     }
-	system("pause");
-	}
 }
 
 int main() {
     fillSlv();
-	gameover = false;
-    
-    for (int line = 0; line < 9; line++) {
+    gameover = false;
+
+    for (int line = 0; line < 4; line++) {
         puts(guiText[line]);
     }
     system("pause");
